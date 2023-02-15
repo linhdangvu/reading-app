e123617998777c29df2a2a88f31be56ae72cbbff
Linh DANG VU <linhdangvu@macbook-pro-de-linh.home>
2023-02-16 00:34:10 +0100

    setup vite vue ionic

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+
+import { IonicVue } from "@ionic/vue";
+
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/vue/css/core.css";
+
+/* Basic CSS for apps built with Ionic */
+import "@ionic/vue/css/normalize.css";
+import "@ionic/vue/css/structure.css";
+import "@ionic/vue/css/typography.css";
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/vue/css/padding.css";
+import "@ionic/vue/css/float-elements.css";
+import "@ionic/vue/css/text-alignment.css";
+import "@ionic/vue/css/text-transformation.css";
+import "@ionic/vue/css/flex-utils.css";
+import "@ionic/vue/css/display.css";
+
+/* Theme variables - copied from existing project */
+import "./theme/variables.css";
+
+// be sure to add the use(IonicVue)
+createApp(App).use(IonicVue).mount("#app");
---
+++ src/router/index.ts
@@ -0,0 +1,4 @@
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes,
+// });
