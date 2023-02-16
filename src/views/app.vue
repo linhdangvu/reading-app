bf49dc158d27bc8fd46ec99d8bdad0f5fe2aef7e
Linh DANG VU <linhdangvu@macbook-pro-de-linh.home>
2023-02-16 01:05:35 +0100

    add xios

--- src/views/app.vue
+++ src/views/app.vue
@@ -1,1 +1,16 @@
+<script setup lang="ts">
+  import axios from "axios";
+
+  const getData = async () => {
+    try {
+      const data = await axios.get("https://gutendex.com/books/1513/");
+      console.log(data);
+    } catch (e: any) {
+      console.log(e);
+    }
+  };
+
+  getData();
+</script>
+
 <template><div>We are in the app</div></template>
